8681e1a211720c99b17f63e3dc4ea9ed34f91729
Karyn Huang <muhua.karyn.huang@gmail.com>
2020-05-02 18:20:41 -0700

    Completed: - Checkbox connnected to the server Incompleted: - Buttons unenabled - No simulation data set

--- app.R
+++ app.R
@@ -11,6 +11,8 @@ library(shiny)
 library(tidyr)
 library(plotly)
 
+
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
@@ -39,7 +41,7 @@ ui <- fluidPage(
                 condition = "input.logistic==true",
                 sliderInput("a2", "Select value for a2",min = -0.8,max = 0.8,value = 0,step = 0.1),
                 sliderInput("b2","Select value for b2",min = -3,max = 3,value = 0,step = 0.1),
-                sliderInput("sd2","Select the standard deviation for y2_data",min = 0.1, max = 2.0,value = 0.1,step = 0.1),
+                sliderInput("sd2","Select the standard deviation for y2_data",min = 0.1, max = 2.0,value = 0.1,step = 0.1)
                 
             ),
                 
@@ -56,153 +58,104 @@ ui <- fluidPage(
             )
         )
     )
-
 server <- function(input, output) {
-    output$linePlot <- renderPlotly({
-#--------------------------------------------------------------------------------------------------------------------#
-# the dataframe
-        x<- seq(-10,10,0.1)
-        y1<- x*input$b1+input$a1
-        y2<- 1/(1+exp(-input$a2-input$b2*x))
-        #data frame for the line plot 
-        df1<-data.frame(x,y1,y2)
-        
-        random_x<- rnorm(input$size,mean = 0, 3)
-        random_num1<- rnorm(input$size, mean=0, sd=input$sd1)
-        random_num2<- rnorm(input$size, mean=0, sd=input$sd2)
-        
-        y1_data<- random_num1 + random_x*input$b1+input$a1
-        y2_data<- 1/(1+exp(random_num2-input$a2-input$b2 * random_x))
-        #data frame for the scatter plot 
-        df2<-data.frame(random_x,y1_data,y2_data)
-        
-        #merging two data frames
-        df3<-merge(df1, df2, by.x = "x", by.y = "random_x", all.x = TRUE,all.y = TRUE)
- #--------------------------------------------------------------------------------------------------------------------#
-        #plot 
-        p<-plot_ly(df3)%>%
-            add_trace(x=x,y=y1,type = 'scatter', mode = 'lines', name = "y1=a1+b1*x",line = list(color = 'rgb(255, 129, 10)')) %>% 
-            add_trace(x=x,y=y2,type = 'scatter', mode = 'lines', name = "y2=1/(exp(-a2-b2*x))", line = list(color = 'rgb(22, 96, 167)'))%>%
-            add_trace(x=round(random_x,4),y= round(y1_data,4), name = 'y1_data',
-                      type = 'scatter',mode = 'markers',marker = list(size = 5, color ='rgb(255, 129, 10)')) %>%
-            add_trace(x=round(random_x,4),y= round(y2_data,4), name = 'y2_data',
-                      type = 'scatter',mode = 'markers',marker = list(size = 5, color ='rgb(22, 96, 167)'))%>%
-            layout(xaxis = list(range=c(-10,10)), yaxis = list(range=c(-32,32)))
-        
-        
+    observeEvent(input$simple||input$logistic,{
+        output$linePlot <- renderPlotly({
+            x<- seq(-10,10,0.1)
+            y1<- x*input$b1+input$a1
+            y2<- 1/(1+exp(-input$a2-input$b2*x))
+            #data frame for the line plot
+            df1<-data.frame(x,y1,y2)
+
+            random_x<- rnorm(input$size,mean = 0, 3)
+            random_num1<- rnorm(input$size, mean=0, sd=input$sd1)
+            random_num2<- rnorm(input$size, mean=0, sd=input$sd2)
+
+            y1_data<- random_num1 + random_x*input$b1+input$a1
+            y2_data<- 1/(1+exp(random_num2-input$a2-input$b2 * random_x))
+            #data frame for the scatter plot
+            df2<-data.frame(random_x,y1_data,y2_data)
+
+            #merging two data frames
+            df3<-merge(df1, df2, by.x = "x", by.y = "random_x", all.x = TRUE,all.y = TRUE)
+
+            p<-plot_ly(df3)%>%
+                layout(xaxis = list(range=c(-10,10)), yaxis = list(range=c(-32,32)))
+            
+
+            #plot
+            if (input$simple){
+                p<-p%>%
+                    add_trace(x=x,y=y1,type = 'scatter', mode = 'lines', name = "y1=a1+b1*x",line = list(color = 'rgb(255, 129, 10)')) 
+                
+            }
+            
+            if (input$logistic) {
+                p<-p%>%
+                    add_trace(x=x,y=y2,type = 'scatter', mode = 'lines', name = "y2=1/(exp(-a2-b2*x))", line = list(color = 'rgb(22, 96, 167)'))
+            }
+            
+            p
+            })
     })
+    
+    # observeEvent(input$logistic,{
+    #     output$linePlot <- renderPlotly({
+    #         
+    #         x<- seq(-10,10,0.1)
+    #         y2<- 1/(1+exp(-input$a2-input$b2*x))
+    #         
+    #         l1<-list(x,y2)
+    #         names(l1)<-c("x","y2")
+    #         df<- as.data.frame(l1)
+    #         
+    #         #plot
+    #         p<-plot_ly(df)%>%
+    #             add_trace(x=x,y=y2,type = 'scatter', mode = 'lines', name = "y2=1/(exp(-a2-b2*x))", line = list(color = 'rgb(22, 96, 167)'))
+    #         
+    #         p
+    #     })
+    # })
 }
-#--------------------------------------------------------------------------------------------------------------------#
-# a funtion to construct a dataframe 
-
-    
-
-
-#--------------------------------------------------------------------------------------------------------------------#
-
-
+#------- GOD VERSION---------------------------------------------------------------------------
+# 
 # server <- function(input, output) {
-#   
-# 
 #     output$linePlot <- renderPlotly({
-#         dfFunction<- function(a1,a2,b1,b2,sd1,sd2,size) {
-#             x<- seq(-10,10,0.1)
-#             y1<- x*b1+a1
-#             y2<- 1/(1+exp(-a2-b2*x))
-#             #data frame for the line plot
-#             df1<-data.frame(x,y1,y2)
-#             
-#             random_x<- rnorm(size,mean = 0, 3)
-#             random_num1<- rnorm(size, mean=0, sd=sd1)
-#             random_num2<- rnorm(size, mean=0, sd=sd2)
-#             
-#             y1_data<- random_num1 + random_x*b1+a1
-#             y2_data<- 1/(1+exp(random_num2-a2-b2 * random_x))
-#             #data frame for the scatter plot
-#             df2<-data.frame(random_x,y1_data,y2_data)
-#             
-#             #merging two data frames
-#             df3<-merge(df1, df2, by.x = "x", by.y = "random_x", all.x = TRUE,all.y = TRUE)
-#             
-#             return (df3)
-#         }
 #         
-#         df<-reactiveValues(df=dfFunction(input$a1,input$a2,input$b1,input$b2,input$sd1,input$sd2,input$size))
-# 
-#         p<- plot_ly(df$df)%>%
-#             add_trace(x=x,y=y1,type = 'scatter', mode = 'lines', name = "y1=a1+b1*x",
-#                       line = list(color = 'rgb(255, 129, 10)'))
-# 
+#         x<- seq(-10,10,0.1)
+#         y1<- x*input$b1+input$a1
+#         y2<- 1/(1+exp(-input$a2-input$b2*x))
+#         #data frame for the line plot 
+#         df1<-data.frame(x,y1,y2)
 #         
-#     #     observeEvent(input$simple,{
-#     #                 p<- plot_ly(df)%>%
-#     #                     add_trace(x=x,y=y1,type = 'scatter', mode = 'lines', name = "y1=a1+b1*x",
-#     #                               line = list(color = 'rgb(255, 129, 10)')) %>%
-#     #                     add_trace(x=x,y=y2,type = 'scatter', mode = 'lines', name = "y2=1/(exp(-a2-b2*x))",
-#     #                               line = list(color = 'rgb(22, 96, 167)'))
-#     #     })
-#     # 
-#     # })
-#    
-#     
-#     # p<- plot_ly(df3)
-#     # 
-#     # observeEvent(input$simple,{
-#     #     output$linePlot <- renderPlotly({
-#     #     p<-p%>%
-#     #         add_trace(x=x,y=y1,type = 'scatter', mode = 'lines', name = "y1=a1+b1*x",
-#     #                                    line = list(color = 'rgb(255, 129, 10)'))})
-#     # })
-#     
-# 
-#     # # clear simulated data points 
-#     # observeEvent(input$clear,{
-#     #     output$linePlot <- renderPlotly({
-#     #         return()
-#     #     })
-#     # })
-#     
-#     # observeEvent(input$simulate,{   
-#     #     
-#     #     output$linePlot <- renderPlotly({
-#     #         
-#     #         
-#     #         x<- seq(-10,10,0.1)
-#     #         y1<- x*input$b1+input$a1
-#     #         y2<- 1/(1+exp(-input$a2-input$b2*x))
-#     #         #data frame for the line plot
-#     #         df1<-data.frame(x,y1,y2)
-#     #         
-#     #         random_x<- rnorm(input$size,mean = 0, 3)
-#     #         random_num1<- rnorm(input$size, mean=0, sd=input$sd1)
-#     #         random_num2<- rnorm(input$size, mean=0, sd=input$sd2)
-#     #         
-#     #         y1_data<- random_num1 + random_x*input$b1+input$a1
-#     #         y2_data<- 1/(1+exp(random_num2-input$a2-input$b2 * random_x))
-#     #         #data frame for the scatter plot
-#     #         df2<-data.frame(random_x,y1_data,y2_data)
-#     #         
-#     #         #merging two data frames
-#     #         df3<-merge(df1, df2, by.x = "x", by.y = "random_x", all.x = TRUE,all.y = TRUE)
-#     #         
-#     #         p<- plot_ly(df3)%>%
-#     #             add_trace(x=x,y=y1,type = 'scatter', mode = 'lines', name = "y1=a1+b1*x",
-#     #                       line = list(color = 'rgb(255, 129, 10)')) %>% 
-#     #             add_trace(x=x,y=y2,type = 'scatter', mode = 'lines', name = "y2=1/(exp(-a2-b2*x))",
-#     #                       line = list(color = 'rgb(22, 96, 167)'))
-#     #         
-#     #         isolate({p<- p%>% 
-#     #             add_trace(x=round(random_x,4),y= round(y1_data,4), name = 'y1_data',
-#     #                       type = 'scatter',mode = 'markers',marker = list(size = 5, color ='rgb(255, 129, 10)')) %>%
-#     #             add_trace(x=round(random_x,4),y= round(y2_data,4), name = 'y2_data',
-#     #                       type = 'scatter',mode = 'markers',marker = list(size = 5, color ='rgb(22, 96, 167)'))%>%
-#     #             layout(xaxis = list(range=c(-10,10)), yaxis = list(range=c(-32,32)))})
-#     #         
-#     #     })
-#     # })
-#         })
-#     }
-    
+#         random_x<- rnorm(input$size,mean = 0, 3)
+#         random_num1<- rnorm(input$size, mean=0, sd=input$sd1)
+#         random_num2<- rnorm(input$size, mean=0, sd=input$sd2)
+#         
+#         y1_data<- random_num1 + random_x*input$b1+input$a1
+#         y2_data<- 1/(1+exp(random_num2-input$a2-input$b2 * random_x))
+#         #data frame for the scatter plot 
+#         df2<-data.frame(random_x,y1_data,y2_data)
+#         
+#         #merging two data frames
+#         df3<-merge(df1, df2, by.x = "x", by.y = "random_x", all.x = TRUE,all.y = TRUE)
+#         
+#         #plot 
+#         p<-plot_ly(df3)%>%
+#             add_trace(x=x,y=y1,type = 'scatter', mode = 'lines', name = "y1=a1+b1*x",line = list(color = 'rgb(255, 129, 10)')) %>% 
+#             add_trace(x=x,y=y2,type = 'scatter', mode = 'lines', name = "y2=1/(exp(-a2-b2*x))", line = list(color = 'rgb(22, 96, 167)'))%>%
+#             add_trace(x=round(random_x,4),y= round(y1_data,4), name = 'y1_data',
+#                       type = 'scatter',mode = 'markers',marker = list(size = 5, color ='rgb(255, 129, 10)')) %>%
+#             add_trace(x=round(random_x,4),y= round(y2_data,4), name = 'y2_data',
+#                       type = 'scatter',mode = 'markers',marker = list(size = 5, color ='rgb(22, 96, 167)'))%>%
+#             layout(xaxis = list(range=c(-10,10)), yaxis = list(range=c(-32,32)))
+#         
+#         p
+#     })
+# }
+
+#------------------------------------------GOD_VERSOIN_IS_OVER----------------------------------
+
 
 
 # Run the application 
